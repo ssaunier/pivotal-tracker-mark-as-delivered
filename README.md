71e48b87007eb5ac08caadb8b2e09b1f42f2a622
Sebastien Saunier <seb@saunier.me>
2014-10-04 15:25:18 +0200

    Forgot the pivotal tracker API token

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ This is a [wercker](https://wercker.com) step to mark
 finished Pivotal Tracker stories as delivered (if the `git log`
 reveals commit messages containing those story ids).
 
+This step uses the [tracker-git](https://github.com/robb1e/tracker-git) gem.
+
 ## Example
 
 If your repo is private, you need to generate a github oauth id
@@ -12,6 +14,7 @@ deploy:
     steps:
         - ssaunier/pivotal-tracker-mark-as-delivered:
             project_id: 1234567
+            token: $TRACKER_API_TOKEN
             github_oauth_token: $GITHUB_OAUTH_TOKEN
 ```
 
--- run.sh
+++ run.sh
@@ -3,11 +3,16 @@ if [ ! -n "$WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_PROJECT_ID" ]; then
   exit 1
 fi
 
+if [ ! -n "$WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_TOKEN" ]; then
+  error 'Please specify your Pivotal Tracker API token (through an environment variable !)'
+  exit 1
+fi
+
 git clone https://$WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_GITHUB_OAUTH_TOKEN:x-oauth-basic@github.com/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY.git /pipeline/repo
 pushd /pipeline/repo
 git checkout -fq $WERCKER_GIT_COMMIT
 git submodule update --init --recursive
 gem install tracker-git -v 0.0.5 --no-ri --no-rdoc
 echo "Tracker project: $WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_PROJECT_ID. Marking finished story with commits as delivered"
-tracker
+tracker $WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_PROJECT_ID $WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_TOKEN
 popd
