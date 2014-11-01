e8e6a3b9f387b17ef36397c6a44320371868ade6
Sebastien Saunier <seb@saunier.me>
2014-11-01 10:40:33 +0100

    Bash scripts are so painful...

--- run.sh
+++ run.sh
@@ -4,12 +4,11 @@ fi
 
 if [ ! -n "$WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_TOKEN" ]; then
   echo 'Please specify your Pivotal Tracker API token (through an environment variable !)'
-fi
-
-if [ -n "$WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_TOKEN"] && [ -n "$WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_PROJECT_ID" ]; then
+else
   pushd $WERCKER_SOURCE_DIR
   git checkout -fq $WERCKER_GIT_COMMIT
   git submodule update --init --recursive
+  echo "Installing tracker-git gem"
   gem install tracker-git -v 0.0.5 --no-ri --no-rdoc
   echo "Tracker project: $WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_PROJECT_ID. Marking finished story with commits as delivered"
   tracker $WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_PROJECT_ID $WERCKER_PIVOTAL_TRACKER_MARK_AS_DELIVERED_TOKEN
